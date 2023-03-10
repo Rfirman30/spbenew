f473ee787c79707cd091031438e9cb7d6219a6b7
Zen <fauz.fauz23@gmail.com>
2023-03-10 13:49:08 +0700

    add fix

--- resources/views/layout/berita.blade.php
+++ resources/views/layout/berita.blade.php
@@ -37,7 +37,7 @@
                         <div class="col-xl-4 col-md-6">
                             <article>
                                 <div class="post-img">
-                                    <img src="{{ url('/image') }}/{{ $item->image }}" alt="" class="img-fluid">
+                                    <img src="{{ url('/image') }}/{{ $item->gambar }}" alt="" class="img-fluid">
                                 </div>
 
                                 <p class="post-category">Public</p>
