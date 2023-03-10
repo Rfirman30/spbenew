1c84bfee1180498863c7ed050da1b0a81adbef00
Zen <fauz.fauz23@gmail.com>
2023-03-10 13:44:32 +0700

    add

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,7 +23,7 @@ class AppServiceProvider extends ServiceProvider
     {
         Schema::defaultStringLength(125);
 
-        if (config('app.debug') != true) {
+        if (config('app.env') == 'production') {
             URL::forceScheme('https');
         }
     }
