872d79a42a8eb3af2ea1f914aa5acf18c943862f
Zen <59063107+fauz2323@users.noreply.github.com>
2023-06-10 18:59:59 +0700

    add question

--- resources/views/layout/tanyaJawab.blade.php
+++ resources/views/layout/tanyaJawab.blade.php
@@ -33,7 +33,34 @@
 
                 <div class="row gy-4 posts-list">
 
-
+                    <form action="" method="post">
+                        @csrf
+                        <div class="row">
+                            <div class="col-5">
+                                <div class="mb-3">
+                                    <label for="exampleFormControlInput1" class="form-label">Email address</label>
+                                    <input type="email" class="form-control" id="exampleFormControlInput1"
+                                        placeholder="name@example.com">
+                                </div>
+                            </div>
+                            <div class="col-5">
+                                <div class="mb-3">
+                                    <label for="exampleFormControlInput1" class="form-label">Name</label>
+                                    <input type="emtextail" class="form-control" id="exampleFormControlInput1"
+                                        placeholder="name@example.com">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="mb-3">
+                            <label for="exampleFormControlInput1" class="form-label">Subjcet</label>
+                            <input type="text" class="form-control" id="exampleFormControlInput1"
+                                placeholder="name@example.com">
+                        </div>
+                        <div class="mb-3">
+                            <label for="exampleFormControlTextarea1" class="form-label">Question</label>
+                            <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                        </div>
+                    </form>
 
                     <!-- End post list item -->
 
